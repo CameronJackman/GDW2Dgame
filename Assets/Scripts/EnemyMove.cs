79063839ab86005373bbed176ba016e8df85ccf2
CameronJackman <cameron.jackman@ontariotechu.net>
2024-11-07 15:35:20 -0500

    Enemy Prefab Finished

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -4,20 +4,38 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
+    private bool move;
+    [SerializeField] private GameObject player, enemy;
+    [SerializeField] private float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        move = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (move == true)
+        {
+            enemy.transform.position = Vector2.MoveTowards(enemy.transform.position, player.transform.position, speed);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.tag == "Player")
+        {
+            move = true; 
+        }
+    }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            move = false;
+        }
     }
 }
